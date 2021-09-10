f2d70e5ef217db1c4b4283bd1802f492b170642d
Andres Reyes <chess.coach.ar@gmail.com>
2021-09-09 22:25:39 -0300

    se dan nombres de clase mas representativos

--- tests/mathematics/AbstractArithmetizable.test.js
+++ tests/mathematics/AbstractArithmetizable.test.js
@@ -1,10 +1,10 @@
 import { AbstractArithmetizable } from "../../lib/b2c6/mathematics/AbstractArithmetizable";
-import { InstanciatingAbstractClassError } from "../../lib/b2c6/errors/Errors";
+import { InstantiatingAbstractClassError } from "../../lib/b2c6/errors/Errors";
 
 test('Testing no instantiating of AbstractArithmetizable', () => {
   expect(
     () => { 
       const abstractInstance = new AbstractArithmetizable(); 
     }
-  ).toThrow(InstanciatingAbstractClassError);
+  ).toThrow(InstantiatingAbstractClassError);
 });
